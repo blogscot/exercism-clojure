50f46ad0a88458c4a46b67c3874f28a3fbb9d957
blogscot <iain.diamond@gmail.com>
2016-12-29 20:24:41 +0000

    Refactor meeting

--- meetup/src/meetup.clj
+++ meetup/src/meetup.clj
@@ -1,28 +1,34 @@
 (ns meetup
   (:import (java.util Calendar)))
 
+(defn- get-day 
+      [schedule days] 
+      (case schedule
+        :first (first days)
+        :second (second days)
+        :third (nth days 2)
+        :fourth (nth days 3)
+        :last (last days)
+        :teenth (first (filter #(and (> % 12) (< % 20)) days))))
+
 (defn meetup
   [month year day schedule]
   (let [weekdays [:sunday :monday :tuesday :wednesday :thursday :friday :saturday]
         calendar (Calendar/getInstance)]
     (.set calendar year (dec month) 1)
     (def last-day (.getActualMaximum calendar (Calendar/DAY_OF_MONTH)))
-    (def day-of-week (.get calendar (Calendar/DAY_OF_WEEK)))
+    (def day-of-week (-> calendar (.get (Calendar/DAY_OF_WEEK)) (dec) ))
 
-    ;; build list of weekdays (:monday, :tuesday, etc.) for each month day
-    (def month-days 
-      (let [dow (dec day-of-week)] 
-        (drop dow (take (+ last-day dow) (cycle weekdays)))))
-    (def with-index (zipmap (range 1 32) month-days))
-    ;; get specified days
-    (def filtered (filter (fn [[k v]] (= day v)) with-index))
-    (def days (sort (keys filtered)))
-    (def result (case schedule
-       :first (first days)
-       :second (second days)
-       :third (nth days 2)
-       :fourth (nth days 3)
-       :last (last days)
-       :teenth (first (filter (fn [x] (and (> x 12) (< x 20))) days))))
-    [year month result]
-))
+    ;; rotate the list of weekdays (:monday, :tuesday, etc.) so that 
+    ;; the weekday falls on the correct date (e.g. 1 = :thursday etc.).
+    (def month-days  
+      (drop day-of-week (take (+ last-day day-of-week) (cycle weekdays))))
+
+    (->> 
+     month-days 
+     (zipmap (range 1 32))
+     (filter (fn [[_ v]] (= day v)))
+     (keys)
+     (sort)
+     (get-day schedule)
+     ((fn [x] [year month x])))))
