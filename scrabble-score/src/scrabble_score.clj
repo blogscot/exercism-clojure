c4a75259905184ce965d581fb7f8bcd976f3631a
blogscot <iain.diamond@gmail.com>
2016-12-30 14:29:58 +0000

    scrabble score

---
+++ scrabble-score/src/scrabble_score.clj
@@ -0,0 +1,16 @@
+(ns scrabble-score
+  (require [clojure.string :refer [lower-case]]))
+
+(def scores (hash-map "a" 1 "e" 1 "i" 1 "o" 1 "u" 1 "l" 1 "n" 1 "r" 1 "s" 1 "t" 1
+                      "d" 2 "g" 2
+                      "b" 3 "c" 3 "m" 3 "p" 3
+                      "f" 4 "h" 4 "v" 4 "w" 4 "y" 4
+                      "k" 5
+                      "j" 8 "x" 8
+                      "q" 10 "z" 10))
+
+(defn score-letter [letter]
+  (-> letter (lower-case) (scores)))
+
+(defn score-word [word]
+  (->> word (seq) (map (comp score-letter str)) (apply +)))
