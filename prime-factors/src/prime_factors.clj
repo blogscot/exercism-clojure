c57178e3ac5a6f18730dac36c2451b3a33864dc5
blogscot <iain.diamond@gmail.com>
2017-01-01 14:12:26 +0000

    Refactor prime factors

--- prime-factors/src/prime_factors.clj
+++ prime-factors/src/prime_factors.clj
@@ -1,13 +1,9 @@
 (ns prime-factors)
 
-(defn do-of [num current acc]
-  (cond
-    (= num 1) []
-    (= 1 (/ num current)) (vec (conj acc current))
-    (zero? (rem num current)) (recur (quot num current) current (conj acc current))
-    :else (recur num (inc current) acc)))
-
 (defn of [num]
-  (do-of num 2 []))
-
-(of 1)
+  (loop [num num current 2 acc []]
+    (cond
+      (= num 1) []
+      (= 1 (/ num current)) (vec (conj acc current))
+      (zero? (rem num current)) (recur (quot num current) current (conj acc current))
+      :else (recur num (inc current) acc))))
