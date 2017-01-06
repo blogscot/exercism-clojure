8b748634e7f8a0120b32a0c82fdf32b90fc89ef7
blogscot <iain.diamond@gmail.com>
2017-01-06 14:32:22 +0000

    Refactor kindergarten garden

--- kindergarten-garden/src/kindergarten_garden.clj
+++ kindergarten-garden/src/kindergarten_garden.clj
@@ -1,22 +1,21 @@
 (ns kindergarten-garden
-  (require [clojure.string :refer [lower-case split]]))
+  (require [clojure.string :refer [lower-case split-lines]]))
 
-(def children '("alice" "bob" "charlie" "david" 
-                "eve" "fred" "ginny" "harriet" 
-                "ileana" "joseph" "kincaid" "larry"))
+(def children '("Alice" "Bob" "Charlie" "David" 
+                "Eve" "Fred" "Ginny" "Harriet" 
+                "Ileana" "Joseph" "Kincaid" "Larry"))
 
-(def flowers {\C :clover \G :grass \R :radishes \V :violets})
+(def plants {\C :clover \G :grass \R :radishes \V :violets})
 
 (defn- zipm [m n] (map #(hash-map %1 %2) m n))
 (defn- update-values [m f & args] (into {} (for [[k v] m] [k (apply f v args)])))
-(defn- parse [m] (update-values m (fn [string] (mapv flowers string))))
-(defn- to-keyword [s] (if (keyword? s) s (keyword s)))
+(defn- parse [m] (update-values m (fn [string] (mapv plants string))))
 
 (defn garden
   ([rows] (garden rows children))
   ([rows students] 
-   (let [[row1 row2] (split rows #"\n")
-         students (map (comp to-keyword lower-case) (sort students))
+   (let [[row1 row2] (split-lines rows)
+         students (map (comp keyword lower-case) (sort students))
          c (flatten (for [child students] [child child]))
-         groups (apply merge-with str (concat (zipm c row1) (zipm c row2)))]
-     (parse groups))))
+         cups (apply merge-with str (concat (zipm c row1) (zipm c row2)))]
+     (parse cups))))
